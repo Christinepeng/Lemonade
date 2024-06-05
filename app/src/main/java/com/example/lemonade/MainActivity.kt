1bf08202fe2861fa80be5ad13543f5d35b34e69b
Yellow Bear <percussionpeng@gmail.com>
2024-06-05 15:07:02 -0700

    update UI: add image background color, clickable image

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,15 +68,30 @@ fun Greeting(modifier: Modifier = Modifier) {
         else -> R.string.tap_the_empty_glass_to_start_again
     }
 
-    Column (
+    Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            painter = painterResource(imageResourse),
-            contentDescription = stringResource(stringResourse)
-        )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(200.dp) // Size of the background square
+                .background(
+                    color = Color(0xFFD1EEC6),
+                    shape = RoundedCornerShape(16.dp)
+                ) // Light green color with rounded corners
+                .padding(16.dp) // Padding inside the background box
+                .clickable {
+                    // Update the state variables when the image is clicked
+                    result = (result % 4) + 1
+                }
+        ) {
+            Image(
+                painter = painterResource(imageResourse),
+                contentDescription = stringResource(stringResourse)
+            )
+        }
         Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = stringResource(stringResourse),
